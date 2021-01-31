ebae4ef6b7591202ba4faec0ab8d13ed2b923ecb
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-01-31 09:57:20 +0100

    simple multithreading

--- src/main/java/multithreading/Demo.java
+++ src/main/java/multithreading/Demo.java
@@ -1,15 +1,29 @@
 package multithreading;
 
-public class Demo {
+import java.time.LocalDate;
+
+public abstract class Demo {
+
+
     public static void main(String[] args) {
+
         //Runnable // zadanie - > kod do wykonania na osobny wątku
         //Thread // pracownik - > wykonuje przekazane zadanie
         Task task = new Task();
-       Thread thread = new Thread(task);
+        Thread thread = new Thread(task);
         thread.start();
-        for (int i = 0; i < 1_000; i++) {
-            System.out.println("Main sam wykonuje prace");
-        }
+
+        Runnable runnable = new Runnable(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 1_000; i++) {
+                    System.out.println("Wątek 2 wykonuje zadanie");
+                }
+            }
+        };
+        Thread thread2 = new Thread(runnable);
+        thread2.start();
+
         System.out.println("Koniec programu!");
     }
 }
---
+++ src/main/java/multithreading/Skroty.java
@@ -0,0 +1,28 @@
+package multithreading;
+
+public class Skroty {
+
+    public static void main(String[] args) {
+        //skróty
+        int[] numbers = {3334, 2, 3334};
+        //iter
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hello");
+        }
+        System.out.println("hello");
+
+        if (numbers[0] > 0) {
+
+        }
+
+        //.var .sout
+
+
+
+
+    }
+}
